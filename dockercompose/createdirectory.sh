f303a07e53c40e44c958152df39ba273eba35584
root <root@MANASA-T7N1NSK.localdomain>
2023-11-04 11:01:51 +0530

    added createdirecctory.sh

---
+++ dockercompose/createdirectory.sh
@@ -0,0 +1,23 @@
+set -e
+
+checkdirectoryname="/home/amujuriv/vivekshell/shell/shellScript"
+
+echo "please enter directory name you wan to create"
+
+presentworkingdirectory=$PWD
+
+echo "currect working directory is : $presentworkingdirectory"
+
+read directoryname
+
+echo "$presentworkingdirectory/$directoryname this is your directory which you are trying to create in this path"
+
+if [ -d "$presentworkingdirectory/$directoryname" ];
+
+then
+	 echo "Directory already exists."
+else
+	echo "Directory $directoryname doesnot exist hence it will be created"
+	mkdir $directoryname
+	echo "directory $directoryname create successfully"
+fi
